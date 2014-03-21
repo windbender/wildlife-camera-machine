bd1cf933e90276cb8a8e1b1c309be7dc9c0f7c46
windbender <windbender@users.noreply.github.com>
2014-03-21 16:22:36 -0700

    whoops should have added this back a few

---
+++ src/main/java/com/github/windbender/domain/Camera.java
@@ -0,0 +1,102 @@
+package com.github.windbender.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+@Entity
+@Table(name="cameras")
+public class Camera {
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@Column(name = "id", nullable=false)
+	Long id;
+	
+	
+	@JsonIgnore
+	@ManyToOne(fetch=FetchType.EAGER)
+	@JoinColumn(name="project_id")
+	Project project;
+
+	@Column(name = "camera_name", nullable=false)
+	String cameraName;
+	
+	public String getCameraName() {
+		return cameraName;
+	}
+
+	public void setCameraName(String cameraName) {
+		this.cameraName = cameraName;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Project getProject() {
+		return project;
+	}
+
+	public void setProject(Project project) {
+		this.project = project;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result
+				+ ((cameraName == null) ? 0 : cameraName.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((project == null) ? 0 : project.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Camera other = (Camera) obj;
+		if (cameraName == null) {
+			if (other.cameraName != null)
+				return false;
+		} else if (!cameraName.equals(other.cameraName))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (project == null) {
+			if (other.project != null)
+				return false;
+		} else if (!project.equals(other.project))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Camera [id=" + id + ", project=" + project + ", cameraName="
+				+ cameraName + "]";
+	}
+	
+
+	
+}
